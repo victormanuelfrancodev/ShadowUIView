ca9e0f47f78560e77b598b9125c97191f2fca2c8
VicktorManuel <victormanuelfrancodev@gmail.com>
2018-07-29 17:08:21 -0500

    Initial Commit

---
+++ ShadowUIView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ShadowUIView
+//
+//  Created by VicktorManuel on 7/29/18.
+//  Copyright © 2018 VicktorManuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
